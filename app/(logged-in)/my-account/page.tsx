a6b02b1ddd50b3b7eb08c29d27c4dbc0b6f7d53c
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-10-01 17:26:17 +0700

    Add My Account Card

--- app/(logged-in)/my-account/page.tsx
+++ app/(logged-in)/my-account/page.tsx
@@ -1,6 +1,23 @@
-const MyAccount = () => {
+import { auth } from '@/auth'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Label } from '@/components/ui/label'
+
+const MyAccount = async () => {
+
+  const session = await auth()
+
   return (
-    <h1>My account</h1>
+    <Card className='w-[350px]'>
+      <CardHeader>
+        <CardTitle>My Account</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Label>Email Address</Label>
+        <div className="text-muted-foreground">
+          {session?.user?.email}
+        </div>
+      </CardContent>
+    </Card>
   )
 }
 
