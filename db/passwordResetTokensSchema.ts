9e17df5731ecd3317a4e1ec494471b00d8bb9b0a
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-10-08 04:16:02 +0700

    Create Password Reset Token table

---
+++ db/passwordResetTokensSchema.ts
@@ -0,0 +1,11 @@
+import { pgTable, serial, text, timestamp, uuid } from "drizzle-orm/pg-core"
+import { users } from "./usersSchema"
+
+export const passwordResetTokens = pgTable("password_reset_token", {
+  id: serial("id").primaryKey(),
+  userId: uuid("user_id").references(() => users.id, {
+    onDelete: "cascade",
+  }),
+  token: text("token"),
+  tokenExpiry: timestamp("token_expiry"),
+})
