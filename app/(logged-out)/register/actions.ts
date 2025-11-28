8f09b160b0f2341b2e7dc969ae604ede6a63ae09
Sekti <14885417+flyingduck92@users.noreply.github.com>
2025-11-29 03:07:30 +0700

    Remove unused db.select()

--- app/(logged-out)/register/actions.ts
+++ app/(logged-out)/register/actions.ts
@@ -1,10 +1,10 @@
-"use server"
+'use server'
 
-import db from "@/db/drizzle"
-import { passwordMatchSchema } from "@/validation/passwordMatchSchema"
-import z from "zod"
-import { hash } from "bcryptjs"
-import { users } from "@/db/usersSchema"
+import db from '@/db/drizzle'
+import { passwordMatchSchema } from '@/validation/passwordMatchSchema'
+import z from 'zod'
+import { hash } from 'bcryptjs'
+import { users } from '@/db/usersSchema'
 
 export const registerUser = async ({
   email,
@@ -15,9 +15,6 @@ export const registerUser = async ({
   password: string
   passwordConfirm: string
 }) => {
-  // connect to db
-  const result = await db.select()
-
   const newUserSchema = z
     .object({
       email: z.email(),
@@ -34,7 +31,7 @@ export const registerUser = async ({
     return {
       error: true,
       message:
-        newUserValidation.error.issues[0]?.message ?? "An error occurred",
+        newUserValidation.error.issues[0]?.message ?? 'An error occurred',
     }
   }
 
@@ -48,17 +45,17 @@ export const registerUser = async ({
     })
   } catch (err: any) {
     // if already registered
-    if (err?.cause?.code === "23505") {
+    if (err?.cause?.code === '23505') {
       return {
         error: true,
-        message: "An account is already registered.",
+        message: 'An account is already registered.',
       }
     }
 
     // if other error (general error)
     return {
       error: true,
-      message: "An error occurred.",
+      message: 'An error occurred.',
     }
   }
 }
